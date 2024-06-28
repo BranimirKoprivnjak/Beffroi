31d82efd0cbbd278aef65ab9a387c129df30d269
BranimirKoprivnjak <koprivnjak965@gmail.com>
2024-06-28 14:52:20 +0200

    custom viewport

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <meta charSet="utf-8" />
         <link
           rel="shortcut icon"
